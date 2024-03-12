4e5d75386df12d8a1456a6e922f0a9824cec04bc
unknown <thanhdinhngoclap123@gmail.com>
2024-03-12 11:07:34 +0700

    triangle

--- InputFromKeyboard.java
+++ InputFromKeyboard.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class InputFromKeyboard {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        System.out.println("How tall are you (m)?");
+        double height = scanner.nextDouble();
+        System.out.println("Mrs/Ms. " + name + ", " + age + " years old. " + "Your height is " + height + ".");
+    }
 }
---
+++ Triangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Triangle {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++){
+            for (int k = 0; k < n - i - 1; k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
