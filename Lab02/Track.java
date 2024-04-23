75283c74e45d6eb26df3a0d8f9da9be309a4d1ac
unknown <thanhdinhngoclap123@gmail.com>
2024-04-23 10:28:51 +0700

    create track class

---
+++ Lab02/Track.java
@@ -0,0 +1,19 @@
+package Lab02;
+
+public class Track {
+    private String title;
+    private String length;
+
+    public Track(String title, String length) {
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLength() {
+        return length;
+    }
+}
