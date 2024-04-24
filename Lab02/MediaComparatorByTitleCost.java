48d6a12cd51b73be602b076d8a300fd63e5a4473
unknown <thanhdinhngoclap123@gmail.com>
2024-04-24 10:00:32 +0700

    implement media comparator

---
+++ Lab02/MediaComparatorByTitleCost.java
@@ -0,0 +1,21 @@
+package Lab02;
+
+import java.util.Comparator;
+
+public class MediaComparatorByTitleCost implements Comparator<Media> {
+    @Override
+    public int compare(Media o1, Media o2) {
+        String o1Title = o1.getTitle();
+        String o2Title = o2.getTitle();
+        if (o1Title.compareTo(o2Title) < 0){
+            return 1;
+        }
+        else if (o1Title.compareTo(o2Title) == 0){
+            if (o1.getCost() <= o2.getCost()) return 1;
+            else return 0;
+        }
+        else {
+            return 0;
+        }
+    }
+}
