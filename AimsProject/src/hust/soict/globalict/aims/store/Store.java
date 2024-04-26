bcb6583aa6a61c0389d6e823380fe9631de852d1
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2024-04-26 08:24:32 +0700

    mid 17 refactor of store

--- AimsProject/src/hust/soict/globalict/aims/store/Store.java
+++ AimsProject/src/hust/soict/globalict/aims/store/Store.java
@@ -1,6 +1,5 @@
 package AimsProject.src.hust.soict.globalict.aims.store;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import AimsProject.src.hust.soict.globalict.aims.Media.DigitalVideoDisc;
 import AimsProject.src.hust.soict.globalict.aims.Media.Media;
@@ -40,41 +39,6 @@ public class Store {
             if (itemsInStore.get(i) instanceof DigitalVideoDisc) {
                 ((DigitalVideoDisc) itemsInStore.get(i)).print();
             }
-        }
-    }
-
-    public void showMenu() {
-        System.out.println("AIMS: ");
-        System.out.println("--------------------------------");
-        System.out.println("1. View store");
-        System.out.println("2. Update store");
-        System.out.println("3. See current cart");
-        System.out.println("0. Exit");
-        System.out.println("--------------------------------");
-        System.out.println("Please choose a number: 0-1-2-3");
-        Scanner scanner = new Scanner(System.in);
-        int result = scanner.nextInt();
-        if (result == 1){
-            this.storeMenu();
-        }
-    }
-    public void storeMenu(){
-        System.out.println("Options: ");
-        System.out.println("--------------------------------");
-        System.out.println("1. See a media’s details");
-        System.out.println("2. Add a media to cart");
-        System.out.println("3. Play a media");
-        System.out.println("4. See current cart");
-        System.out.println("0. Back");
-        System.out.println("--------------------------------");
-        System.out.println("Please choose a number: 0-1-2-3-4");
-        Scanner scanner = new Scanner(System.in);
-        int result = scanner.nextInt();
-        if (result == 0){
-            this.showMenu();
-        }
-        else if (result == 1){
-            return;
         }
     }
 }
