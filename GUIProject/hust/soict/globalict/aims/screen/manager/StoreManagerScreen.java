1f3a70e7291fed029985d79512ce4b28cba1e244
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2024-05-13 10:32:20 +0700

    3.1.1:finish

---
+++ GUIProject/hust/soict/globalict/aims/screen/manager/StoreManagerScreen.java
@@ -0,0 +1,7 @@
+package GUIProject.hust.soict.globalict.aims.screen.manager;
+
+import AimsProject.src.hust.soict.globalict.aims.store.Store;
+
+public class StoreManagerScreen {
+    private Store store;
+}
