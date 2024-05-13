9f3e2ffb793de7b86023fa73b7631ead25862fe5
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2024-05-13 11:32:10 +0700

    3.2:add button listner

--- GUIProject/hust/soict/globalict/aims/screen/manager/StoreManagerScreen.java
+++ GUIProject/hust/soict/globalict/aims/screen/manager/StoreManagerScreen.java
@@ -11,10 +11,12 @@ import javax.swing.Box;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
-public class StoreManagerScreen extends JFrame {
+public class StoreManagerScreen extends JFrame implements ActionListener {
     private Store store;
     public StoreManagerScreen(Store store){
         this.store = store;
@@ -24,6 +26,7 @@ public class StoreManagerScreen extends JFrame {
         cp.add(createCenter(), BorderLayout.CENTER);
 
         setTitle("Store");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1024, 768);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -36,14 +39,23 @@ public class StoreManagerScreen extends JFrame {
         return north;
     }
     public JMenuBar createMenuBar(){
-        JMenu menu = new JMenu("Options");
+        JMenuItem view = new JMenuItem("View store");
+        JMenuItem addBook = new JMenuItem("Add Book");
+        JMenuItem addCD = new JMenuItem("Add CD");
+        JMenuItem addDVD = new JMenuItem("Add DVD");
+        view.addActionListener(this);
+        addBook.addActionListener(this);
+        addCD.addActionListener(this);
+        addDVD.addActionListener(this);
 
-        menu.add(new JMenuItem("View store"));
+        JMenu menu = new JMenu("Options");
+        menu.add(view);
 
         JMenu smUpdateStore = new JMenu("Update Store");
-        smUpdateStore.add(new JMenuItem("Add Book"));
-        smUpdateStore.add(new JMenuItem("Add CD"));
-        smUpdateStore.add(new JMenuItem("Add DVD"));
+        smUpdateStore.add(addBook);
+        smUpdateStore.add(addCD);
+        smUpdateStore.add(addDVD);
+
         menu.add(smUpdateStore);
         JMenuBar menuBar = new JMenuBar();
         menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -80,5 +92,20 @@ public class StoreManagerScreen extends JFrame {
     }
     public static void main(String[] args){
         new StoreManagerScreen(new Store());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text = ((JMenuItem)e.getSource()).getText();
+        switch(text){
+            case "View store":
+                System.out.println("viewing store");break;
+            case "Add Book":
+                System.out.println("Adding book");break;
+            case "Add CD":
+                System.out.println("Adding CD");break;
+            case "Add DVD":
+                System.out.println("Adding DVD");break;
+        }
     }
 }
