6831e74a510d10b6e7e3851057c0f76ce4fdf2b9
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2024-05-13 11:03:56 +0700

    3.1.5: finish

---
+++ GUIProject/hust/soict/globalict/aims/screen/manager/MediaStore.java
@@ -0,0 +1,38 @@
+package GUIProject.hust.soict.globalict.aims.screen.manager;
+
+import AimsProject.src.hust.soict.globalict.aims.Media.Media;
+import AimsProject.src.hust.soict.globalict.aims.Playable.Playable;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class MediaStore extends JPanel {
+    private Media media;
+    public MediaStore(Media media){
+        this.media = media;
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(media.getTitle());
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 15));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel cost = new JLabel("" + media.getCost() + "$");
+        cost.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel container = new JPanel();
+        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        if (media instanceof Playable){
+            JButton playButton = new JButton("Play");
+            container.add(playButton);
+        }
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(cost);
+        this.add(Box.createVerticalGlue());
+        this.add(container);
+
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+}
--- GUIProject/hust/soict/globalict/aims/screen/manager/StoreManagerScreen.java
+++ GUIProject/hust/soict/globalict/aims/screen/manager/StoreManagerScreen.java
@@ -1,7 +1,84 @@
 package GUIProject.hust.soict.globalict.aims.screen.manager;
 
+import AimsProject.src.hust.soict.globalict.aims.Media.Media;
 import AimsProject.src.hust.soict.globalict.aims.store.Store;
 
-public class StoreManagerScreen {
+import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.Box;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+public class StoreManagerScreen extends JFrame {
     private Store store;
+    public StoreManagerScreen(Store store){
+        this.store = store;
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(createNorth(), BorderLayout.NORTH);
+        cp.add(createCenter(), BorderLayout.CENTER);
+
+        setTitle("Store");
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    public JPanel createNorth(){
+        JPanel north = new JPanel();
+        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+        north.add(createMenuBar());
+        north.add(createHeader());
+        return north;
+    }
+    public JMenuBar createMenuBar(){
+        JMenu menu = new JMenu("Options");
+
+        menu.add(new JMenuItem("View store"));
+
+        JMenu smUpdateStore = new JMenu("Update Store");
+        smUpdateStore.add(new JMenuItem("Add Book"));
+        smUpdateStore.add(new JMenuItem("Add CD"));
+        smUpdateStore.add(new JMenuItem("Add DVD"));
+        menu.add(smUpdateStore);
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        menuBar.add(menu);
+        return menuBar;
+    }
+    public JPanel createHeader(){
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JLabel title = new JLabel("AIMS");
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+        title.setForeground(Color.CYAN);
+
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+        header.add(title);
+        header.add(Box.createHorizontalGlue());
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+
+        return header;
+    }
+    public JPanel createCenter(){
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(3, 2, 2, 2));
+
+        ArrayList<Media> mediaInStore = store.getItemsInStore();
+        if (!mediaInStore.isEmpty()){
+            for (int i = 0; i < 9; i++){
+                MediaStore cell = new MediaStore(mediaInStore.get(i));
+                center.add(cell);
+            }
+        }
+        return center;
+    }
+    public static void main(String[] args){
+        new StoreManagerScreen(new Store());
+    }
 }
